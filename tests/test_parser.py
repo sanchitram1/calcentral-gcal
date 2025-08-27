bf8833692109505f93fe2ddbcdf2a0c56b4da8bd
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-08-26 23:47:01 -0700

    testing for parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,25 +1,38 @@
-from parser import parse_class_schedule
+from parser import Course, parse_class_schedule
 
 
-def main():
+def test_parse_class_schedule():
     schedule_test = """Skip to Main Content
     Schedule Planner
     Help Help
     Potential Schedule for 2025 Fall
-    Status	Class #	Subject	Course	Section	Seats Open	Instruction Mode	Instructor	Day(s) & Location(s)	Units
-    Enrolled	29901	Industrial Eng & Ops Rsch	215	001	20	In-Person Instruction	
-    Phillip Kerger
+    Status\tClass #\tSubject\tCourse\tSection\tSeats Open\tInstruction Mode\tInstructor\tDay(s) & Location(s)\tUnits
+    Enrolled\t29901\tIndustrial Eng & Ops Rsch\t215\t001\t20\tIn-Person Instruction\t\n    Phillip Kerger
     MW 12:00pm - 12:59pm - Latimer 120
     3
     Has Reserved SeatsHas Reserved Seats
     Reserved Seats:
     130 of 150 At least some seats in this class are reserved for students who meet specific criteria. See the Berkeley Academic Guide Class Schedule for details.
-    Enrolled	33001	Industrial Eng & Ops Rsch	215	102	18	In-Person Instruction	
-    F 10:00am - 10:59am - Barker 101
+    Enrolled\t33001\tIndustrial Eng & Ops Rsch\t215\t102\t18\tIn-Person Instruction\t\n    F 10:00am - 10:59am - Barker 101
     0"""
 
-    print(parse_class_schedule(schedule_test))
-
-
-if __name__ == "__main__":
-    main()
+    result = parse_class_schedule(schedule_test)
+    expected = [
+        Course(
+            id="29901",
+            name="Industrial Eng & Ops Rsch",
+            number="215",
+            location="Latimer 120",
+            schedule={"Days": "MW", "Time": "12:00pm - 12:59pm"},
+            instructor=["Phillip Kerger"],
+        ),
+        Course(
+            id="33001",
+            name="Industrial Eng & Ops Rsch",
+            number="215",
+            location="Barker 101",
+            schedule={"Days": "F", "Time": "10:00am - 10:59am"},
+            instructor=[],
+        ),
+    ]
+    assert result == expected
